1f68f05a7ffc077b159ba732bcef5d8777a53021
Ronak Ganatra <20298819+ronakganatra@users.noreply.github.com>
2024-06-09 12:53:33 +0200

    chore: dotenv and codeowners

--- src/config/posthog.js
+++ src/config/posthog.js
@@ -1,5 +1,6 @@
 import posthog from 'posthog-js';
+require('dotenv').config();
 
-posthog.init('your-api-key', { api_host: 'https://app.posthog.com' });
+posthog.init(process.env.POSTHOG_API_KEY, { api_host: 'https://app.posthog.com' });
 
 export default posthog;
